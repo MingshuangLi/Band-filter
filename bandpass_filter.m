69d938cf1c0fcf8e8341524408ca296402f06679
Mingshuang Li <44843611+MingshuangLi@users.noreply.github.com>
2020-06-07 14:19:12 -0500

    Add files via upload

---
+++ bandpass_filter.m
@@ -0,0 +1,11 @@
+% Band filter for imported sounds (e.g., speech and noise)
+% Mingshuang Li, UT Austin
+clear;
+
+wavefile = input('Please type the wave file name: ', 's');
+[waveform, fs] = audioread(wavefile);
+low_cutoff = input('Please input the low cut off frequency (Hz): ');
+high_cutoff = input('Please input the high cut off frequency (Hz): ');
+waveform_banded = bandpass(waveform,[low_cutoff high_cutoff],fs);
+names= input('Please type name of new audiofile: ', 's');
+audiowrite(names,waveform_banded,fs)
