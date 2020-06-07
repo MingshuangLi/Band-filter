78ee3587f9d45d4e3a05e00f29bcde5157ed8ebb
Mingshuang Li <44843611+MingshuangLi@users.noreply.github.com>
2020-06-07 16:41:24 -0500

    Add files via upload

---
+++ high_pass_filter.m
@@ -0,0 +1,13 @@
+% High pass filter for imported sounds (e.g., speech and noise)
+% Mingshuang Li, UT Austin
+clear;
+
+wavefile = input('Please type the wave file name: ', 's');
+cutoff = input('Please input the high-pass cut off frequency (Hz): ');
+steepness = input('Please input the steepness ratio: ');
+[waveform, fs] = audioread(wavefile);
+[waveform_LP,d] = highpass(waveform,cutoff,fs,'Steepness',steepness);
+figure;
+pspectrum(waveform_LP,fs)
+names= input('Please type name of new audiofile: ', 's');
+audiowrite(names,waveform_LP,fs)
