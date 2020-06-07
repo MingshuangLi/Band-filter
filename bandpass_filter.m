3d56e4d8ef4a40ed8c351a5870a9f927e22c42bb
Mingshuang Li <44843611+MingshuangLi@users.noreply.github.com>
2020-06-07 16:47:11 -0500

    Update bandpass_filter.m

--- bandpass_filter.m
+++ bandpass_filter.m
@@ -7,5 +7,7 @@ wavefile = input('Please type the wave file name: ', 's');
 low_cutoff = input('Please input the low cut off frequency (Hz): ');
 high_cutoff = input('Please input the high cut off frequency (Hz): ');
 waveform_banded = bandpass(waveform,[low_cutoff high_cutoff],fs);
+figure;
+pspectrum(waveform_banded,fs);
 names= input('Please type name of new audiofile: ', 's');
 audiowrite(names,waveform_banded,fs)
